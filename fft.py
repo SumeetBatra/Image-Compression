32748b91d8503213c47560f81f046b6f6ebb2a95
Sumeet Batra <sumeetbatra@cu-engr2-1-10-10.201.38.200.int.colorado.edu>
2018-12-10 16:45:38 -0700

    added fft recursive

--- fft.py
+++ fft.py
@@ -27,21 +27,58 @@ def DFT(x):
     return M.dot(x)
 
 
-#Fast fourier transform
-def FFT(x):
-    pass
+#Fast fourier transform (my own implementation) NOT WORKING xD
+def FFTSlow(x):
+    x = np.asarray(x, dtype=float)
+    x_even = x[::2].reshape(-1, 1)
+    x_odd = x[1::2].reshape(-1, 1)
+    N = x.shape[0]
+    m = np.arange((N/2))
+    k = np.arange(N).reshape(-1, 1)
 
-#Inverse fast fourier transform - return back from frequency space 
+    if np.log2(N) % 1 > 0:
+        raise ValueError("size of x must be a power of 2")
+
+    M_even = np.exp(-2j * math.pi * k * m / (N/2))
+    result_left = M_even.dot(x_even)
+    M_odd = np.exp(-2j* math.pi * k * m / (N/2))
+    result_right = np.exp(-2j * math.pi * k / N) * M_odd.dot(x_odd)
+    return result_left + result_right
+
+#recursively implemented FFT
+def FFT(x):
+    """A recursive implementation of the 1D Cooley-Tukey FFT"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+
+    if N % 2 > 0:
+        raise ValueError("size of x must be a power of 2")
+    elif N <= 32:  # this cutoff should be optimized
+        return DFTSlow(x)
+    else:
+        X_even = FFT(x[::2])
+        X_odd = FFT(x[1::2])
+        factor = np.exp(-2j * np.pi * np.arange(N) / N)
+        return np.concatenate([X_even + factor[:N / 2] * X_odd,
+                               X_even + factor[N / 2:] * X_odd])
+
+
+#Inverse fast fourier transform - return back from frequency space
 def IFFT(x):
-    pass
+    x_conj = np.conj(x)
+    x_new = np.fft.fft(x_conj)
+    x_final = float((1.0 / len(x))) * np.conj(x_new)
+    return x_final
 
 
 if __name__ == '__main__':
     #Naive implementation of fourier transform, check to see that it works with random input
-    x = np.random.randn(10)
+    x = np.random.randn(16)
     y = DFTSlow(x)
     print(np.allclose(y, np.fft.fft(x)))
     print(np.allclose(DFT(x), np.fft.fft(x)))
+    print(np.allclose(FFT(x), np.fft.fft(x)))
+
 
     #actual image compression stuff
     img = cv2.imread('cat.jpg')
